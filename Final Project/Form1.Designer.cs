bebb2af49a1400a422630900b8a0771552d25c75
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2024-12-05 20:10:09 +0530

    login half finished:

--- Final Project/Form1.Designer.cs
+++ Final Project/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Final_Project
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,16 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.txtpassword = new System.Windows.Forms.TextBox();
             this.txtusername = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.btnlogin = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -52,19 +51,55 @@
             this.panel1.Controls.Add(this.txtpassword);
             this.panel1.Controls.Add(this.txtusername);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(735, 163);
+            this.panel1.Location = new System.Drawing.Point(653, 66);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(638, 591);
+            this.panel1.Size = new System.Drawing.Size(478, 480);
             this.panel1.TabIndex = 0;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlogin.Location = new System.Drawing.Point(186, 426);
+            this.btnlogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(135, 36);
+            this.btnlogin.TabIndex = 5;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(195, 338);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(186, 235);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "User Name";
             // 
             // txtpassword
             // 
             this.txtpassword.BackColor = System.Drawing.SystemColors.Window;
             this.txtpassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtpassword.Location = new System.Drawing.Point(104, 454);
+            this.txtpassword.Location = new System.Drawing.Point(78, 369);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtpassword.Name = "txtpassword";
-            this.txtpassword.Size = new System.Drawing.Size(455, 38);
+            this.txtpassword.Size = new System.Drawing.Size(342, 32);
             this.txtpassword.TabIndex = 2;
             // 
             // txtusername
@@ -72,9 +107,10 @@
             this.txtusername.BackColor = System.Drawing.SystemColors.Window;
             this.txtusername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtusername.Location = new System.Drawing.Point(104, 328);
+            this.txtusername.Location = new System.Drawing.Point(78, 266);
+            this.txtusername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtusername.Name = "txtusername";
-            this.txtusername.Size = new System.Drawing.Size(455, 41);
+            this.txtusername.Size = new System.Drawing.Size(342, 35);
             this.txtusername.TabIndex = 1;
             this.txtusername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txtusername.TextChanged += new System.EventHandler(this.txtusername_TextChanged);
@@ -83,9 +119,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(259, 205);
+            this.label1.Location = new System.Drawing.Point(194, 167);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 38);
+            this.label1.Size = new System.Drawing.Size(85, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Login";
             // 
@@ -93,60 +130,37 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.Black;
             this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Location = new System.Drawing.Point(735, 163);
+            this.panel2.Location = new System.Drawing.Point(653, 66);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(638, 186);
+            this.panel2.Size = new System.Drawing.Size(478, 151);
             this.panel2.TabIndex = 0;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(170, 23);
+            this.pictureBox1.Image = global::Final_Project.Properties.Resources.WhatsApp_Image_2024_11_07_at_09_30_23_20f03f32_1;
+            this.pictureBox1.Location = new System.Drawing.Point(128, 19);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(319, 131);
+            this.pictureBox1.Size = new System.Drawing.Size(239, 106);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // label2
+            // Login
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(242, 284);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(164, 32);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "User Name";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(242, 406);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(146, 32);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Password";
-            // 
-            // btnlogin
-            // 
-            this.btnlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnlogin.Location = new System.Drawing.Point(248, 524);
-            this.btnlogin.Name = "btnlogin";
-            this.btnlogin.Size = new System.Drawing.Size(180, 44);
-            this.btnlogin.TabIndex = 5;
-            this.btnlogin.Text = "Login";
-            this.btnlogin.UseVisualStyleBackColor = true;
-            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1437, 872);
+            this.BackgroundImage = global::Final_Project.Properties.Resources.Blue_Modern_Login_Page_Desktop_Prototype_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1224, 622);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Name = "Form1";
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
--- Final Project/Form1.cs
+++ Final Project/Form1.cs
@@ -11,9 +11,9 @@ using System.Windows.Forms;
 
 namespace Final_Project
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
@@ -25,15 +25,80 @@ namespace Final_Project
 
         private void btnlogin_Click(object sender, EventArgs e)
         {
-            //connection
-            string cs = @"Data Source=HPNotebook;
-                        Initial Catalog=DSE_FinalProject;
-                        Integrated Security=True";
-            SqlConnection conn = new SqlConnection(cs);
-            conn.Open();
+            if(txtusername.Text == "")
+            {
+                MessageBox.Show("Enter the Username");
+            }
+            else if(txtpassword.Text == "")
+            {
+                MessageBox.Show("Enter the Password");
+            }
+            else
+            {
+                string usertype = string.Empty;
 
-            //command
-            string sql = "SELECT Employee FROM "
+                //connection string
+                string cs = @"Data Source=HPNotebook; Initial Catalog=DSE_FinalProject; Integrated Security=True";
+                using (SqlConnection conn = new SqlConnection(cs))
+                {
+                    try
+                    {
+                        // Open connection
+                        conn.Open();
+
+                        // command
+                        string sql = "SELECT empType FROM Employee WHERE empUserName=@username AND empPassword=@password";
+                        SqlCommand com = new SqlCommand(sql, conn);
+                        com.Parameters.AddWithValue("@username", this.txtusername.Text);
+                        com.Parameters.AddWithValue("@password", this.txtpassword.Text);
+
+                        // access Data 
+                        SqlDataReader dr = com.ExecuteReader();
+
+                        if (dr.Read())
+                        {
+                            usertype = dr.GetValue(0).ToString();
+
+                            // Validate user type and open respective dashboard
+                            if (usertype == "Admin")
+                            {
+                                // open admin dashboard
+                                MessageBox.Show("Successful login as Admin");
+                            }
+                            else if (usertype == "SalesPerson")
+                            {
+                                // open salesperson dashboard
+                                MessageBox.Show("Successful login as SalesPerson");
+                            }
+                            else if (usertype == "Technician")
+                            {
+                                // open technician dashboard
+                                MessageBox.Show("Successful login as Technician");
+                            }
+                            else
+                            {
+                                // Handle unknown user type
+                                MessageBox.Show("Unknown user type");
+                            }
+                        }
+                        else
+                        {
+
+                            MessageBox.Show("Invalid username or password");
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message);
+                    }
+                    finally
+                    {
+                        //close connection
+                        conn.Close();
+                    }
+                }
+            }
         }
     }
 }
