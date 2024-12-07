07d5b5d1860b7561184d7b803c6a9accaea37dbe
unknown <harinduvihan@gmail.com>
2024-12-07 12:02:51 +0530

    jobNote Done

---
+++ Final Project/Technician  Jobnotes.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Final_Project
+{
+    partial class Technician__Jobnotes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.txtDT = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.boxCusId = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(573, 46);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 22);
+            this.txtId.TabIndex = 42;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(368, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 16);
+            this.label5.TabIndex = 41;
+            this.label5.Text = "Job Note id";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(573, 327);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 40;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(368, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 16);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "Issued date and time";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(368, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "description";
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(573, 198);
+            this.txtDesc.Multiline = true;
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(177, 79);
+            this.txtDesc.TabIndex = 32;
+            // 
+            // txtDT
+            // 
+            this.txtDT.Enabled = false;
+            this.txtDT.Location = new System.Drawing.Point(573, 283);
+            this.txtDT.Name = "txtDT";
+            this.txtDT.Size = new System.Drawing.Size(177, 22);
+            this.txtDT.TabIndex = 43;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(368, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 16);
+            this.label2.TabIndex = 44;
+            this.label2.Text = "Customer id";
+            // 
+            // boxCusId
+            // 
+            this.boxCusId.FormattingEnabled = true;
+            this.boxCusId.Location = new System.Drawing.Point(573, 93);
+            this.boxCusId.Name = "boxCusId";
+            this.boxCusId.Size = new System.Drawing.Size(121, 24);
+            this.boxCusId.TabIndex = 45;
+            this.boxCusId.SelectedIndexChanged += new System.EventHandler(this.boxCusId_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(368, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 16);
+            this.label3.TabIndex = 46;
+            this.label3.Text = "Customer Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Enabled = false;
+            this.txtName.Location = new System.Drawing.Point(573, 155);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 47;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Enabled = false;
+            this.btnPrint.Location = new System.Drawing.Point(573, 356);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 48;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            // 
+            // Technician__Jobnotes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.boxCusId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDT);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDesc);
+            this.Name = "Technician__Jobnotes";
+            this.Text = "Technician__Jobnotes";
+            this.Load += new System.EventHandler(this.Technician__Jobnotes_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.TextBox txtDT;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox boxCusId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnPrint;
+    }
+}
---
+++ Final Project/Technician  Jobnotes.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace Final_Project
+{
+    public partial class Technician__Jobnotes : Form
+    {
+        public Technician__Jobnotes()
+        {
+            InitializeComponent();
+        }
+
+        private void IdAutoIncrement()
+        {
+            try
+            {
+                // Create a connection
+                string cs = "Data Source=LAPTOP-DOH91PI2;Initial Catalog=DSE_FinalProject;Integrated Security=True";
+
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    con.Open();
+
+                    string sql1 = "SELECT MAX(jobNoteId) FROM JobNote";
+                    using (SqlCommand cmd = new SqlCommand(sql1, con))
+                    {
+                        SqlDataReader dr = cmd.ExecuteReader();
+
+                        if (dr.Read())
+                        {
+                            string maxItemId = dr[0]?.ToString(); // Use null-conditional operator to handle null values
+                            Console.WriteLine($"maxItemId: {maxItemId}"); // Debug output
+
+                            if (!string.IsNullOrEmpty(maxItemId) && maxItemId.StartsWith("JN"))
+                            {
+                                // Remove the prefix 'JN' and parse the numeric part
+                                string numericPart = maxItemId.Substring(2);
+                                if (int.TryParse(numericPart, out int maxID))
+                                {
+                                    // Increment the numeric part
+                                    int newID = maxID + 1;
+                                    // Format the new ID back to string with 'JN' prefix
+                                    this.txtId.Text = "JN" + newID.ToString();
+                                }
+                                else
+                                {
+                                    // Handle the case where the numeric part is not valid
+                                    this.txtId.Text = "JN1";
+                                }
+                            }
+                            else
+                            {
+                                // Handle the case where there are no records in the table or invalid format
+                                this.txtId.Text = "JN1";
+                            }
+                        }
+                        else
+                        {
+                            // Handle the case where there are no records in the table
+                            this.txtId.Text = "JN1";
+                        }
+                        dr.Close(); // Ensure the data reader is closed
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Something went wrong: " + ex.Message, "Information");
+            }
+        }
+
+        private void getValuesToboxCusId()
+        {
+            try
+            {
+                //Create a connection
+
+                string cs = "Data Source = LAPTOP-DOH91PI2;Initial Catalog = DSE_FinalProject; Integrated Security = True ";
+                SqlConnection con = new SqlConnection(cs);
+
+                con.Open();
+
+                //Define a command
+
+                String sql = "SELECT cusId FROM Customer";
+
+                SqlCommand cmd = new SqlCommand(sql, con);
+               
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    boxCusId.Items.Add(dr["cusId"].ToString());
+
+                }
+                dr.Close();
+                con.Close();
+            }
+            catch
+            {
+                MessageBox.Show(" Something went wrong ", "Information");
+            }
+        }
+
+        private void Technician__Jobnotes_Load(object sender, EventArgs e)
+        {
+            IdAutoIncrement();
+            this.txtDT.Text = DateTime.Now.ToString();
+            getValuesToboxCusId();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //checking any empty values
+
+            if (txtDesc.Text == "")
+            {
+                MessageBox.Show("Enter a Description");
+            }          
+            else
+            {
+
+                /* try
+                 {*/
+                //Create a connection
+
+                string cs = "Data Source = LAPTOP-DOH91PI2;Initial Catalog = DSE_FinalProject; Integrated Security = True ";
+                SqlConnection con = new SqlConnection(cs);
+
+                con.Open();
+
+                //Define a command
+
+                string sql = "INSERT INTO JobNote (jobNoteId,description,dateTime,cusId) VALUES (@jobNoteId,@description,@dateTime,@cusId)";
+
+                SqlCommand com = new SqlCommand(sql, con);
+
+                com.Parameters.AddWithValue("@jobNoteId", this.txtId.Text);
+                com.Parameters.AddWithValue("@description", this.txtDesc.Text);
+                com.Parameters.AddWithValue("@dateTime", this.txtDT.Text);
+                com.Parameters.AddWithValue("@cusId", this.boxCusId.Text);
+
+
+                //Execute the command
+                com.ExecuteNonQuery();
+                MessageBox.Show("Jobe Note Saved Succesfully ", "Information");
+
+
+                IdAutoIncrement();
+                this.txtDT.Text = DateTime.Now.ToString();
+
+                //Disconnect from the sql server
+                con.Close();
+
+                txtDesc.Text = "";
+                boxCusId.Text = "";
+
+                btnPrint.Enabled = true;
+
+                /*}
+                catch (SqlException)
+                {
+                    MessageBox.Show(" Something went wrong ", "Information");
+                }*/
+
+            }
+        }
+
+        private void boxCusId_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /* try
+            {*/
+                //Create a connection
+
+                string cs = "Data Source = LAPTOP-DOH91PI2;Initial Catalog = DSE_FinalProject; Integrated Security = True ";
+                SqlConnection con = new SqlConnection(cs);
+
+                con.Open();
+
+                //Define a command
+
+                String sql = "SELECT cusName FROM Customer Where cusId = @cusId";
+
+                SqlCommand cmd = new SqlCommand(sql, con);
+
+                cmd.Parameters.AddWithValue("@cusId", this.boxCusId.Text);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+
+                this.txtName.Text = dr.GetValue(0).ToString();
+
+                dr.Close();
+                con.Close();
+            /*}
+            catch (SqlException)
+            {
+                MessageBox.Show(" Something went wrong ", "Information");
+            }*/
+        }
+    }
+}
