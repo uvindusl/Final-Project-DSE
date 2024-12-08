db4f428be0b5a73055be558de11332c01ee40087
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2024-12-08 20:39:34 +0530

    add new forms

---
+++ Final Project/Admin view service.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Project
+{
+    public partial class Admin_view_service : Form
+    {
+        public Admin_view_service()
+        {
+            InitializeComponent();
+        }
+
+        private void txtproduct_TextChanged(object sender, EventArgs e)
+        {
+            //connection
+            string cs = @"Data Source=HPNotebook; 
+                        Initial Catalog=DSE_FinalProject; 
+                        Integrated Security=True";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+
+            //commands
+            string sql = "SELECT *From Service WHERE serviceId=@id";
+            SqlCommand com = new SqlCommand(sql, con);
+            com.Parameters.AddWithValue("@id", this.txtproduct.Text);
+
+            //Data Adaptor
+            SqlDataAdapter dap = new SqlDataAdapter(com);
+            DataSet ds = new DataSet();
+            dap.Fill(ds);
+            this.dataGridView1.DataSource = ds.Tables[0];
+
+            //Disconnect
+            con.Close();
+        }
+
+        private void Admin_view_service_Load(object sender, EventArgs e)
+        {
+            //connection
+            string cs = @"Data Source=HPNotebook; 
+                        Initial Catalog=DSE_FinalProject; 
+                        Integrated Security=True";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+
+            //commands
+            string sql = "SELECT *From Service";
+            SqlCommand com = new SqlCommand(sql, con);
+
+            //access data using data adapter method
+            SqlDataAdapter dap = new SqlDataAdapter(com);
+            DataSet ds = new DataSet();
+            dap.Fill(ds);
+
+            //blind data with controls
+            this.dataGridView1.DataSource = ds.Tables[0];
+
+            //disconnect
+            con.Close();
+        }
+
+        private void btnreset_Click(object sender, EventArgs e)
+        {
+            //connection
+            string cs = @"Data Source=HPNotebook; 
+                        Initial Catalog=DSE_FinalProject; 
+                        Integrated Security=True";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+
+            //commands
+            string sql = "SELECT *From Service";
+            SqlCommand com = new SqlCommand(sql, con);
+
+            //access data using data adapter method
+            SqlDataAdapter dap = new SqlDataAdapter(com);
+            DataSet ds = new DataSet();
+            dap.Fill(ds);
+
+            //blind data with controls
+            this.dataGridView1.DataSource = ds.Tables[0];
+
+            //disconnect
+            con.Close();
+        }
+    }
+}
---
+++ Final Project/Technician View,search Job note.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Project
+{
+    public partial class Technician_View_search_Job_note : Form
+    {
+        public Technician_View_search_Job_note()
+        {
+            InitializeComponent();
+        }
+
+        private void Technician_View_search_Job_note_Load(object sender, EventArgs e)
+        {
+            //connection
+            string cs = @"Data Source=HPNotebook; 
+                        Initial Catalog=DSE_FinalProject; 
+                        Integrated Security=True";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+
+            //commands
+            string sql = "SELECT *From JobNote";
+            SqlCommand com = new SqlCommand(sql, con);
+
+            //access data using data adapter method
+            SqlDataAdapter dap = new SqlDataAdapter(com);
+            DataSet ds = new DataSet();
+            dap.Fill(ds);
+
+            //blind data with controls
+            this.dataGridView1.DataSource = ds.Tables[0];
+
+            //disconnect
+            con.Close();
+        }
+
+        private void btnreset_Click(object sender, EventArgs e)
+        {
+            //connection
+            string cs = @"Data Source=HPNotebook; 
+                        Initial Catalog=DSE_FinalProject; 
+                        Integrated Security=True";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+
+            //commands
+            string sql = "SELECT *From JobNote";
+            SqlCommand com = new SqlCommand(sql, con);
+
+            //access data using data adapter method
+            SqlDataAdapter dap = new SqlDataAdapter(com);
+            DataSet ds = new DataSet();
+            dap.Fill(ds);
+
+            //blind data with controls
+            this.dataGridView1.DataSource = ds.Tables[0];
+
+            //disconnect
+            con.Close();
+        }
+
+        private void txtproduct_TextChanged(object sender, EventArgs e)
+        {
+            //connection
+            string cs = @"Data Source=HPNotebook; 
+                        Initial Catalog=DSE_FinalProject; 
+                        Integrated Security=True";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+
+            //commands
+            string sql = "SELECT *From JobNote WHERE jobNoteId=@id";
+            SqlCommand com = new SqlCommand(sql, con);
+            com.Parameters.AddWithValue("@id", this.txtproduct.Text);
+
+            //Data Adaptor
+            SqlDataAdapter dap = new SqlDataAdapter(com);
+            DataSet ds = new DataSet();
+            dap.Fill(ds);
+            this.dataGridView1.DataSource = ds.Tables[0];
+
+            //Disconnect
+            con.Close();
+        }
+    }
+}
